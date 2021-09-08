40ff8d47a6a17d4dd80dfb79ab84b4f627794c4a
Muhammad Shohibul Faqih <70243455+sanzking@users.noreply.github.com>
2021-09-08 22:23:44 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,25 +1,10 @@
-import requests
-import json
 from flask import Flask
-
-url = "http://www.sanzstore.org/api/covid"
-html = requests.get(url).text
-raw = json.loads(html)
-positif = raw["result"]["positif"]
-meninggal = raw["result"]["meninggal"]
-sembuh = raw["result"]["sembuh"]
-
-data = {
-    "positif":positif,
-    "meninggal":meninggal,
-    "sembuh":sembuh
-}
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return data
+    return "hai cok"
 
 if __name__ == '__main__':
     app.run()
