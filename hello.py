7a2c04ac395deb5312eaa6283780ee9c8b4b8da3
Muhammad Shohibul Faqih <70243455+sanzking@users.noreply.github.com>
2021-09-08 21:58:21 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,30 @@
+from bs4 import BeautifulSoup as bs
+import requests as req
+import json as js
 from flask import Flask
+
+url = "http://www.sanzstore.org/api/covid"
+html = req.get(url).text
+json = js.loads(html)
+positif = json["result"]["positif"]
+meninggal = json["result"]["meninggal"]
+sembuh = json["result"]["sembuh"]
+print(positif)
+print(meninggal)
+print(sembuh)
+
+json_data = {
+    "positif":positif,
+    "meninggal":meninggal,
+    "sembuh":sembuh
+}
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return json_data
 
 if __name__ == '__main__':
     app.run()
