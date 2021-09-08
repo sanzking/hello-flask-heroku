ebe2104501d86c2ee95c6479c21d5d3203b48044
Muhammad Shohibul Faqih <70243455+sanzking@users.noreply.github.com>
2021-09-09 02:37:49 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,28 +1,13 @@
-import requests as req
-import json as js
+from api import covid
 from flask import Flask
 
-url = "http://www.sanzstore.org/api/covid"
-html = req.get(url).text
-json = js.loads(html)
-positif = json["result"]["positif"]
-meninggal = json["result"]["meninggal"]
-sembuh = json["result"]["sembuh"]
 
-json_data = {
-	"creator":"sanzking",
-	"status":200,
-	"result":{
-    	"positif":positif,
-   	 "meninggal":meninggal,
-    	"sembuh":sembuh
-    }
-}
 
 app = Flask(__name__)
-@app.route('/api')
+
+@app.route('/api/covid')
 def hai():
-	return json_data
+	return covid
 
 if __name__ == "__main__":
     app.run()
